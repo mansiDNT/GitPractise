d7d24cb7c9d6fffd00bdcf4a6316d36f9cc287e8
BAPS <BAPS@DESKTOP-MKTSV3B>
2021-07-19 17:31:29 +0530

    commit 1

--- src/Git/gitPractise.java
+++ src/Git/gitPractise.java
@@ -5,7 +5,7 @@ public class gitPractise {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("testing in mansi branch");
+		System.out.println("testing in mansi");
 	}
 
 }
