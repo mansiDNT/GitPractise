464de893294be9fcb312b43a4b01a7168c56085e
BAPS <BAPS@DESKTOP-MKTSV3B>
2021-07-19 18:02:38 +0530

    commit for mansi

--- src/Git/gitPractise.java
+++ src/Git/gitPractise.java
@@ -5,9 +5,8 @@ public class gitPractise {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-
-		System.out.println("testing in kalpesh");
-		System.out.println("commiting this to take copy of code in kalpesh branch");
+		System.out.println("testing in mansi");
+	
 
 	}
 
